c44e01bdf9c09c26fec80dbc821493171e23141f
Louis Hache <hache.louis.dev@gmail.com>
2019-02-11 12:51:06 +0100

    theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         name: `The Better Play`,
         short_name: `tbp`,
         start_url: `/`,
-        background_color: `#d74669`,
+        background_color: `#2a079b`,
         theme_color: `#d74669`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
