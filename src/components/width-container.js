e6a75afb53910656abc27d3377dff73c1dd467ec
Louis Hache <hache.louis.dev@gmail.com>
2019-03-07 11:19:11 +0100

    ssr width container

--- src/components/width-container.js
+++ src/components/width-container.js
@@ -2,13 +2,17 @@ import React from 'react'
 import {Box} from '@rebass/grid'
 import SSRRenderer from './ssr-renderer';
 
-const WidthContainerMobile = (props) => <Box width="1" my={0} mx="auto"/>
-const WidthContainerDesktop = (props) => <Box width="3/4" my={0} mx="auto"/>
+const WidthContainerMobile = (props) => <Box width="1" my={0} mx="auto" {...props}/>
+const WidthContainerDesktop = (props) => <Box width="3/4" my={0} mx="auto" {...props}/>
 
 const WidthContainer = (props) => (
     <SSRRenderer
-        mobileComponent={< WidthContainerMobile />}
-        desktopComponent={< WidthContainerDesktop />}/>
+        mobileComponent={< WidthContainerMobile {
+        ...props
+    } />}
+        desktopComponent={< WidthContainerDesktop {
+        ...props
+    } />}/>
 )
 
 export default WidthContainer
